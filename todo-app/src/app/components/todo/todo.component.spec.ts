9f78734f7a578156967e03b05a7e395788cdc356
hide <hide@example.com>
2025-11-12 00:34:29 +0900

    ** 초기설정 수정

--- todo-app/src/app/components/todo/todo.component.spec.ts
+++ todo-app/src/app/components/todo/todo.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Todo } from './todo';
+import { TodoComponent } from './todo.component';
 
-describe('Todo', () => {
-  let component: Todo;
-  let fixture: ComponentFixture<Todo>;
+describe('TodoComponent', () => {
+  let component: TodoComponent;
+  let fixture: ComponentFixture<TodoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Todo]
+      declarations: [ TodoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Todo);
+    fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ todo-app/src/app/components/todo/todo.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  todos: string[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
