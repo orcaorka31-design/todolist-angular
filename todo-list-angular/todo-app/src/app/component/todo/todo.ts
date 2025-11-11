cebd76ada130c322bcee96ba448c998038720633
hide <hide@example.com>
2025-11-12 00:16:01 +0900

    ** 초기커밋

---
+++ todo-list-angular/todo-app/src/app/component/todo/todo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  imports: [],
+  templateUrl: './todo.html',
+  styleUrl: './todo.css',
+})
+export class TodoComponent {
+}
