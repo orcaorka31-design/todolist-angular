a1da05e521346b4a88672507ed2b0f46cfcb443b
hide <hide@example.com>
2025-11-17 00:17:06 +0900

    ** 투두리스트 로컬스토리지 저장 기능 추가

--- todo-app/src/app/components/todo/todo.component.ts
+++ todo-app/src/app/components/todo/todo.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   todos: string[] = [];
   newTodo: string = '';
 
+  ngOnInit() {
+    // 페이지 로드 시 LocalStorage에서 데이터 불러오기
+    const saved = localStorage.getItem('todos');
+    if (saved) {
+      this.todos = JSON.parse(saved);
+    }
+  }
+
   addTodo() {
-    if (this.newTodo.trim() !== '') {
+    if (this.newTodo.trim()) {
       this.todos.push(this.newTodo.trim());
       this.newTodo = '';
+      this.saveToLocalStorage();
     }
   }
 
   deleteTodo(index: number) {
     this.todos.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
